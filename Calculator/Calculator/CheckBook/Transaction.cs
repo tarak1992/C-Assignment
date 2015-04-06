894228742d56f3299cf5e302e5cfac343af397ac
jewpaltz <plotkinm@newpaltz.edu>
2015-04-06 09:18:40 -0400

    Forms Over Data

--- Calculator/Calculator/CheckBook/Transaction.cs
+++ Calculator/Calculator/CheckBook/Transaction.cs
@@ -9,6 +9,13 @@ namespace Calculator.CheckBook
 {
     public class Transaction: BaseVM
     {
+        private CheckBookVM _VM;
+        public CheckBookVM VM
+        {
+            get { return _VM; }
+            set { _VM = value; OnPropertyChanged(); }
+        }
+        
         private DateTime _Date;
         public DateTime Date
         {
@@ -27,7 +34,7 @@ namespace Calculator.CheckBook
         public string Account
         {
             get { return _Account; }
-            set { _Account = value; OnPropertyChanged(); }
+            set { _Account = value; OnPropertyChanged(); if (VM != null) VM.OnPropertyChanged("Accounts"); }
         }
 
         private double _Amount;
@@ -53,24 +60,30 @@ namespace Calculator.CheckBook
         public ObservableCollection<Transaction> Transactions
         {
             get { return _Transactions; }
-            set { _Transactions = value; OnPropertyChanged(); }
+            set { _Transactions = value; OnPropertyChanged(); OnPropertyChanged("Accounts"); }
         }
+
+        public IEnumerable<string> Accounts
+        {
+            get { return Transactions.Select(t=> t.Account).Distinct(); }
+        }
+        
 
         public void Fill()
         {
             Transactions = new ObservableCollection<Transaction>( new[] {
-                new Transaction { Date= DateTime.Now.AddDays(-1), Account="Checking", Payee="Moshe", Amount=30, Tag="Food" },
-                new Transaction { Date= DateTime.Now.AddDays(-2), Account="Checking", Payee="Bracha", Amount=30.5, Tag="Food" },
-                new Transaction { Date= DateTime.Now.AddDays(-3), Account="Checking", Payee="Tim", Amount=130, Tag="Auto" },
-                new Transaction { Date= DateTime.Now.AddDays(-4), Account="Checking", Payee="Moshe", Amount=35, Tag="Food" },
-                new Transaction { Date= DateTime.Now.AddDays(-5), Account="Checking", Payee="Bracha", Amount=35, Tag="Food" },
-                new Transaction { Date= DateTime.Now.AddDays(-6), Account="Checking", Payee="Tim", Amount=20, Tag="Auto" },
-                new Transaction { Date= DateTime.Now.AddDays(-1), Account="Credit", Payee="Moshe", Amount=30, Tag="Food" },
-                new Transaction { Date= DateTime.Now.AddDays(-2), Account="Credit", Payee="Bracha", Amount=30.5, Tag="Food" },
-                new Transaction { Date= DateTime.Now.AddDays(-3), Account="Credit", Payee="Tim", Amount=130, Tag="Auto" },
-                new Transaction { Date= DateTime.Now.AddDays(-4), Account="Credit", Payee="Moshe", Amount=35, Tag="Food" },
-                new Transaction { Date= DateTime.Now.AddDays(-5), Account="Credit", Payee="Bracha", Amount=35, Tag="Food" },
-                new Transaction { Date= DateTime.Now.AddDays(-6), Account="Credit", Payee="Tim", Amount=20, Tag="Auto" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-1), Account="Checking", Payee="Moshe", Amount=30, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-3), Account="Checking", Payee="Tim", Amount=130, Tag="Auto" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-4), Account="Checking", Payee="Moshe", Amount=35, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-5), Account="Checking", Payee="Bracha", Amount=35, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-6), Account="Checking", Payee="Tim", Amount=20, Tag="Auto" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-1), Account="Credit", Payee="Moshe", Amount=30, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-2), Account="Credit", Payee="Bracha", Amount=30.5, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-3), Account="Credit", Payee="Tim", Amount=130, Tag="Auto" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-4), Account="Credit", Payee="Moshe", Amount=35, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-5), Account="Credit", Payee="Bracha", Amount=35, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-2), Account="Checking", Payee="Bracha", Amount=30.5, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-6), Account="Credit", Payee="Tim", Amount=20, Tag="Auto" },
             });
         }
     }
